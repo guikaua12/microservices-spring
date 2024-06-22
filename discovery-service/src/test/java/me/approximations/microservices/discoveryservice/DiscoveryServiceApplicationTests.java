a5f68d599f3e7698f2dbc5b7c59528475498bd46
Approximations <datkidbosst@gmail.com>
2024-06-22 12:51:02 -0300

    feat: added discovery-service

---
+++ discovery-service/src/test/java/me/approximations/microservices/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.approximations.microservices.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
