b9b8a6f57dcb6c6b2ed274d65c1623d88b391483
Approximations <datkidbosst@gmail.com>
2024-06-22 12:51:16 -0300

    feat: added eureka discovery client on both services

--- address-service/src/main/java/me/approximations/microservices/addressservice/AddressServiceSpringApplication.java
+++ address-service/src/main/java/me/approximations/microservices/addressservice/AddressServiceSpringApplication.java
@@ -2,8 +2,10 @@ package me.approximations.microservices.addressservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AddressServiceSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(AddressServiceSpringApplication.class);
--- employee-service/src/main/java/me/approximations/microservices/employeeservice/clients/AddressServiceClient.java
+++ employee-service/src/main/java/me/approximations/microservices/employeeservice/clients/AddressServiceClient.java
@@ -3,6 +3,6 @@ package me.approximations.microservices.employeeservice.clients;
 import me.approximations.microservices.api.core.address.AddressResource;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="address-service", url="http://localhost:8081/address")
+@FeignClient(name="address-service", path="/address")
 public interface AddressServiceClient extends AddressResource {
 }
