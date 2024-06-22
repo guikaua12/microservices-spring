a260930c1d649b67496328d7a5bfda3c1a1e9c49
Approximations <datkidbosst@gmail.com>
2024-06-22 18:02:59 -0300

    fix: fixed eureka clients not being registered if server.port is 0

--- address-service/src/main/java/me/approximations/microservices/addressservice/controllers/AddressController.java
+++ address-service/src/main/java/me/approximations/microservices/addressservice/controllers/AddressController.java
@@ -1,6 +1,7 @@
 package me.approximations.microservices.addressservice.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.approximations.microservices.addressservice.entities.Address;
 import me.approximations.microservices.addressservice.services.AddressService;
 import me.approximations.microservices.api.core.address.AddressDTO;
@@ -15,23 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/address")
+@Slf4j
 public class AddressController implements AddressResource {
     private final AddressService addressService;
     private final ModelMapper modelMapper;
 
     @Override
     public AddressDTO findById(@PathVariable("id") Long id) {
+        log.debug("request hit");
         return modelMapper.map(addressService.findById(id), AddressDTO.class);
     }
 
     @Override
     public Page<AddressDTO> findAll(Pageable pageable) {
+        log.debug("request hit");
         final Page<Address> addresses = addressService.findAll(pageable);
         return addresses.map(address -> modelMapper.map(address, AddressDTO.class));
     }
 
     @Override
     public AddressDTO findByUserId(@PathVariable("userId") Long id) {
+        log.debug("request hit");
         return modelMapper.map(addressService.findByUserId(id), AddressDTO.class);
     }
 }
